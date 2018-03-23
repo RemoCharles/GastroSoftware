b2dd5e781e220e65bf6e91f619a1b508ab0a72d7
Joshua Hemmings <josh@hemmings.ch>
2018-03-23 15:57:23 +0100

    Menukarte Klasse erstellt

--- src/main/java/slgp/gastrosoftware/zentrale/persister/domain/Menukarte.java
+++ src/main/java/slgp/gastrosoftware/zentrale/persister/domain/Menukarte.java
@@ -1,4 +1,50 @@
 package slgp.gastrosoftware.zentrale.persister.domain;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Menukarte {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String bezeichnung;
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Esswaren> listeEsswaren;
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Getraenke> listeGetraenke;
+
+    public Menukarte(){
+
+    }
+    public Menukarte(List<Getraenke> listeGetraenke, List<Esswaren> listeEsswaren, String bezeichnung){
+        this.listeGetraenke = listeGetraenke;
+        this.listeEsswaren = listeEsswaren;
+        this.bezeichnung = bezeichnung;
+
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public List<Esswaren> getListeEsswaren() {
+        return listeEsswaren;
+    }
+
+    public void setListeEsswaren(List<Esswaren> listeEsswaren) {
+        this.listeEsswaren = listeEsswaren;
+    }
+
+    public List<Getraenke> getListeGetraenke() {
+        return listeGetraenke;
+    }
+
+    public void setListeGetraenke(List<Getraenke> listeGetraenke) {
+        this.listeGetraenke = listeGetraenke;
+    }
 }
