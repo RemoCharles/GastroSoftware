6ab092c7adb1ca70bcd17edae8decdeb15e9276c
Joshua Hemmings <josh@hemmings.ch>
2018-03-30 16:35:04 +0200

    Projektstruktur aktualisiert

--- src/main/java/slgp/gastrosoftware/zentrale/verwaltung/api/GetraenkeDAO.java
+++
@@ -1,7 +0,0 @@
-package slgp.gastrosoftware.zentrale.verwaltung.api;
-
-import slgp.gastrosoftware.zentrale.persister.domain.Getraenke;
-
-public interface GetraenkeDAO<T> extends KonsumartikelDAO<Getraenke> {
-
-}
