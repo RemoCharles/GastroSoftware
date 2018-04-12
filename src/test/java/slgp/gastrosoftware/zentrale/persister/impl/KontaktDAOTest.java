b065b463fb39497cca9ebd661fc8ed45036179db
Stefan Ilic <stefan.ilic@stud.hslu.ch>
2018-04-12 16:49:44 +0200

    KotaktDAOTest anpassung

--- src/test/java/slgp/gastrosoftware/zentrale/persister/impl/KontaktDAOTest.java
+++ src/test/java/slgp/gastrosoftware/zentrale/persister/impl/KontaktDAOTest.java
@@ -19,7 +19,7 @@ import slgp.gastrosoftware.zentrale.persister.api.KontaktDAO;
 import slgp.gastrosoftware.zentrale.persister.domain.Kontakt;
 import slgp.gastrosoftware.zentrale.persister.util.JpaUtil;
 
-public class KontaktDAOTest {
+public class s {
 	
 	private static KontaktDAO kKontakt = new KontaktDAOImpl();
 	
