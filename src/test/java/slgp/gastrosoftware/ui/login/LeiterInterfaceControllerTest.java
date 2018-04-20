06784cf1b1cc77b92b374029fe0d8f14f3bda5c5
Remo Charles Gemperli <remo.gemperli@hotmail.ch>
2018-04-20 12:23:13 +0200

    LeiterInterfaceControllerTest Link zu LeiterMenu angepasst

--- src/test/java/slgp/gastrosoftware/ui/login/LeiterInterfaceControllerTest.java
+++ src/test/java/slgp/gastrosoftware/ui/login/LeiterInterfaceControllerTest.java
@@ -20,11 +20,11 @@ public class LeiterInterfaceControllerTest implements Initializable {
 	}
 	@FXML
 	private void menuAnzeigen(ActionEvent event) throws Exception {
-		Parent saal1_interface_parent = FXMLLoader.load(getClass().getResource("/fxml/Saal1.fxml"));
-		Scene saal1_interface_scene = new Scene(saal1_interface_parent);
-		Stage saal1_stage = (Stage)  ((Node) event.getSource()).getScene().getWindow();
-		saal1_stage.setScene(saal1_interface_scene);
-		saal1_stage.show();
+		Parent leiter_menu_parent = FXMLLoader.load(getClass().getResource("/fxml/LeiterMenu.fxml"));
+		Scene leiter_menu_scene = new Scene(leiter_menu_parent);
+		Stage leiter_stage = (Stage)  ((Node) event.getSource()).getScene().getWindow();
+		leiter_stage.setScene(leiter_menu_scene);
+		leiter_stage.show();
 	}
 	
 	@FXML
