913725a8a350502d898fa7932196e6d2f2924c6d
DavidFenk <david.fenk@stud.hslu.ch>
2018-05-09 14:57:44 +0200

    Table für zubereitete Bestellpositionen implementiert inkl. autom. Aktualisierung

--- gastrosoftware-gui-service-leiter/src/main/java/slgp/gastrosoftware/gui/controller/MaInterfaceController.java
+++ gastrosoftware-gui-service-leiter/src/main/java/slgp/gastrosoftware/gui/controller/MaInterfaceController.java
@@ -2,8 +2,17 @@ package slgp.gastrosoftware.gui.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,16 +25,71 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import slgp.gastrosoftware.model.BestellPosition;
+import slgp.gastrosoftware.model.Konsumartikel;
+import slgp.gastrosoftware.persister.BestellPositionDAO;
+import slgp.gastrosoftware.persister.impl.BestellPositionDAOImpl;
+
+import javax.xml.bind.annotation.XmlList;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 
 public class MaInterfaceController implements Initializable{
 
+	@FXML
+	private TableView<BestellPosition> tblBestellungen;
+
+	@FXML
+	private Button btnServiert;
+
+	@FXML
+	private TableColumn<BestellPosition, String> colBez;
+
+	@FXML
+	private TableColumn<BestellPosition, Integer> colTisch;
+
+	private static final Logger logger = LogManager.getLogger(MaInterfaceController.class);
+	private static BestellPositionDAO bestellPositionDAO = new BestellPositionDAOImpl();
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-
+		colBez.setCellValueFactory(new PropertyValueFactory<BestellPosition, String>("bezeichnung"));
+		colTisch.setCellValueFactory(new PropertyValueFactory<BestellPosition, Integer>("tischNummer"));
+		tabelleFuellen();
+		tabelleAktualisieren();
 	}
-	
-	
+
+	@FXML
+	private void serviert(ActionEvent event) throws Exception{
+		BestellPosition bP = tblBestellungen.getSelectionModel().getSelectedItem();
+		bP.setZubereitet(false);
+		bestellPositionDAO.update(bP);
+		tabelleFuellen();
+	}
+
+	private void tabelleFuellen() {
+		try {
+			List<BestellPosition> bestellPositionList = bestellPositionDAO.findAll();
+			List<BestellPosition> offeneBestellungenList = new ArrayList<>();
+			for (BestellPosition bP : bestellPositionList) {
+				if (bP.getZubereitet()) {
+					offeneBestellungenList.add(bP);
+				}
+			}
+			ObservableList<BestellPosition> offeneBestellungenObsv = FXCollections.observableArrayList(offeneBestellungenList);
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    tblBestellungen.setItems(offeneBestellungenObsv);
+                }
+            });
+
+		}catch(Exception e){
+			logger.info("Tabelle konnte nicht befèllt werden.");
+		}
+	}
+
 	@FXML
 	private void saal1Action(ActionEvent event) throws Exception {
 		Parent saal1_interface_parent = FXMLLoader.load(getClass().getResource("/fxml/Saal1.fxml"));
@@ -52,6 +116,22 @@ public class MaInterfaceController implements Initializable{
 		login_stage.setScene(login_interface_scene);
 		login_stage.show();
 	}
-	
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    public void tabelleAktualisieren() {
+        final Runnable aktualisieren = new Runnable() {
+            public void run() {
+                try {
+                    tabelleFuellen();
+                    System.out.println("aktualisiert");
+                } catch (Exception e) {
+                    logger.info("Tabelle konnte nicht befüllt werden...");
+                }
+            }
+        };
+        final ScheduledFuture<?> aktualisierungsHandle = scheduler.scheduleAtFixedRate(aktualisieren, 5, 5, SECONDS);
+
+    }
 
 }
