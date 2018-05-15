5aed365f0b3b5a2c3d2dba582667347dc1b81f78
Joshua Hemmings <josh@hemmings.ch>
2018-05-16 01:28:03 +0200

    GUI schliessen beendet Thread

--- gastrosoftware-gui-kueche/src/main/java/KuecheApplication.java
+++ gastrosoftware-gui-kueche/src/main/java/KuecheApplication.java
@@ -1,9 +1,12 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 
@@ -18,6 +21,14 @@ public class KuecheApplication extends Application {
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.getIcons().add(new Image("restaurant.png"));
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) throws Exception {
--- gastrosoftware-gui-service-leiter/src/main/java/slgp/gastrosoftware/gui/ServiceLeiterApplication.java
+++ gastrosoftware-gui-service-leiter/src/main/java/slgp/gastrosoftware/gui/ServiceLeiterApplication.java
@@ -1,11 +1,14 @@
 package slgp.gastrosoftware.gui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class ServiceLeiterApplication extends Application {
 
@@ -17,6 +20,14 @@ public class ServiceLeiterApplication extends Application {
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.getIcons().add(new Image("restaurant.png"));
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) throws Exception {
